b592fb1326d41b448a6428b3764a4849708eddd7
Lerato <lertodubej@gmail.com>
2023-03-28 13:04:18 -0700

    fixing errors

--- _printf.c
+++ _printf.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
@@ -7,8 +8,14 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
-int flags, width, precision, size, buff_ind = 0;
+int i;
+int printed = 0;
+int printed_chars = 0;
+int flags;
+int width;
+int precision;
+int size;
+int buff_ind = 0;
 va_list list;
 char buffer[BUFF_SIZE];
 if (format == NULL)
